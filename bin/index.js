fedc464ea2e1a2a0e7b4aec8dd0549b3598fdda5
Nicolas Bazille <oltodo@msn.com>
2019-11-29 11:46:43 +0100

    Export parseFile function

--- bin/index.js
+++ bin/index.js
@@ -4,10 +4,10 @@ function escapeString(str) {
   return str.replace(/("|`)/g, "\\$1");
 }
 
-function parse(code) {
+function execute(arg) {
   var bin = __dirname + "/qml-parser.app/Contents/MacOS/qml-parser";
 
-  var result = execSync(bin + ' "' + escapeString(code) + '"');
+  var result = execSync(bin + " " + arg);
   var ast = JSON.parse(result);
 
   if (ast === null) {
@@ -17,6 +17,15 @@ function parse(code) {
   return ast;
 }
 
+function parse(code) {
+  return execute('"' + escapeString(code) + '"');
+}
+
+function parseFile(filepath) {
+  return execute(filepath);
+}
+
 module.exports = {
-  parse: parse
+  parse: parse,
+  parseFile: parseFile
 };
